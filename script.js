68ec28d9a523bfb941b207854a054e934e3509b0
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-04 09:36:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+const div = document.querySelector(".div")
+const img1 = document.querySelectorAll("img")
+// console.log(img1[0]==img1[0])
+div.addEventListener("load",()=>{
+	let x = img1[0] 
+img1[0] = img1[1]
+img1[1] = x
+console.log(img1)
+})
