eba338bc05d793c9f736a73a990ac8734cab76ee
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-04 19:26:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,45 @@
 const body = document.querySelector("body")
 const div = document.querySelector(".div")
 const img1 = document.querySelectorAll("img")
+const reset = document.querySelector("#reset")
+const verify = document.querySelector("#verify")
+const para = document.querySelector("#para")
+reset.style.visibility = "hidden"
+verify.style.visibility = "hidden"
 let a = Math.round(Math.random()*5)
 if(a==0){      
 	a = 1
 }
-let x = document.createElement("img")
+let x = document.createElement("img")                           
      x.className = `img${a}`               
-	div.insertBefore(x,img1[a])
-let a = []
-div.addEventListener("click",(e)=>{
-	 a = e.target.className
-	 
-	// console.log(e.target==e.target)
+	div.insertBefore(x,img1[a])  
+   
+let b = [] 
+let clickHandler = (e)=>{
+	para.textContent = ""
+	b.push(e.target.className)
+	 if(b.length==1){
+		 reset.style.visibility = "visible"
+	 } 
+	if(b.length==2){
+		div.removeEventListener("click",clickHandler)
+		reset.style.visibility = "hidden"
+		verify.style.visibility = "visible"        
+	} 
+} 
+div.addEventListener("click", clickHandler)
+verify.addEventListener("click",()=>{
+	if(b[0]==b[1]){
+		para.textContent = "You are a human. Congratulations!"
+	}else{
+		para.textContent = "We can't verify you as a human. You selected the non-identical tiles"
+		div.addEventListener("click", clickHandler)
+		b = []
+	}
+	verify.style.visibility = "hidden" 
+}) 
+reset.addEventListener("click",()=>{ 
+	reset.style.visibility = "hidden"  
+	b = []
+	console.log()
 })
-console.log(a)
-console.log(b)
